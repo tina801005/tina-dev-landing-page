242583d86fa4a18d6018b138d94f468be76f4f1f
Tina Wang <t870018t@gmail.com>
2026-06-12 13:21:02 +0800

    20260612 -theHeader製作開始! 1. 建立StatusLogo.vue 用來製作可顯示狀態的動態logo 2. 新增 composables資料夾，用於管理交互邏輯 3. 新增時間偵測邏輯 useStudioStatus.ts

---
+++ src/components/ui/StatusLogo.vue
@@ -0,0 +1,11 @@
+<!-- logo
+    a. 點擊回首頁
+    b. 顯示在線狀態10:00-18:00顯示綠色圓點為在線，其他時間及星期二顯示灰色原點表示離線或忙碌中
+    c. 與footer的logo共用，所以除了logo單獨拆出component之外，連ts時間檢查邏輯也要拆出composables -->
+<script setup lang="ts">
+</script>
+
+<template>
+</template>
+
+<style scoped></style>
